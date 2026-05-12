7a16f926968ee2fa0ad9288f241007b357adbccf
alejandraamparo11-crypto <alejandraamparo11@gmail.com>
2026-05-12 19:56:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from telegram import Update
 
-TOKEN = 8727058246:AAEFrkEm8ArS3MIoIy8ldAdMJhVPczzUyVs
+TOKEN = "8727058246:AAEFrkEm8ArS3MIoIy8ldAdMJhVPczzUyVs"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("🦋 Bot encendido. Bienvenida a Moonlight Bloom 🦋")
